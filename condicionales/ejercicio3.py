f18390df672724accdeb00e9f6f1a04e8779b7a8
Tanner <qtulises@gmail.com>
2020-12-20 18:44:39 -0600

    Se añadio el codigo del ejercicio 3

---
+++ condicionales/ejercicio3.py
@@ -0,0 +1,7 @@
+#Programa para saber si una division es exacta o no
+n= float(input("Introduce el divisor: "))
+m= float(input("Introduce el dividendo: "))
+if m==0:
+    print("Error no se puede dividir por 0.")
+else:
+    print(n/m)
